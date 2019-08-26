62d0b0b23190136114634d8d55a6501f2d7f09ae
Nicholas WM <nicholaswesleymazzei@gmail.com>
2019-08-26 00:19:25 -0300

    feat: start text robot

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const readline = require("readline-sync")
+const robots = {
+    // userInput: require('./robots/user-input.js'),
+    text: require('./robots/text.js')
+}
 
 
-function start() {
+async function start() {
     const content = {}
     
     content.searchTerm = askAndReturnSearchItem()
     content.prefix = askAndReturnPrefix()
+
+    // robots.userInput(content)
+    await robots.text(content)
 
     function askAndReturnSearchItem() {
         return readline.question('Type a Wikipedia search term: ')
@@ -15,14 +22,10 @@ function start() {
         const prefixes = ['Who is', 'The history is', 'The history of']
         const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
         const selectedPrefixText = prefixes[selectedPrefixIndex]
-        console.log(selectedPrefixText);
+        // console.log(selectedPrefixText);
 
-        return selectedPrefixText
-        
+        return selectedPrefixText 
     }
-
-    console.log(content);
-    
+    console.log(content)
 }
-
 start()
